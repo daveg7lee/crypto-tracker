dbe9e8f22f1ca629be1e84633075c4607f16898e
Dave <daveg7lee@gmail.com>
2022-01-29 00:10:58 +0900

    Final

--- src/routes/Price.tsx
+++ src/routes/Price.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import styled, { keyframes } from 'styled-components';
+import styled from 'styled-components';
 
 const Container = styled.div`
   display: flex;
