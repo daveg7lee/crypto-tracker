b93944abd37da3cf9bd2e72587bb74874696df39
Dave <daveg7lee@gmail.com>
2022-01-29 00:23:02 +0900

    coin -> coin/chart

--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -89,7 +89,10 @@ function Coins() {
           {data?.slice(0, 100).map((coin) => (
             <Coin key={coin.id}>
               <Link
-                to={{ pathname: `/${coin.id}`, state: { name: coin.name } }}
+                to={{
+                  pathname: `/${coin.id}/chart`,
+                  state: { name: coin.name },
+                }}
               >
                 <Img
                   src={`https://cryptoicon-api.vercel.app/api/icon/${coin.symbol.toLowerCase()}`}
